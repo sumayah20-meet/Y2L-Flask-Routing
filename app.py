12638baa36412e78f0fbab894f6f3b2caf56e888
myname21-meet <myname21@meet.mit.edu>
2019-12-19 11:45:02 -0500

    almost done

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, request, redirect, url_for, render_template
 from flask import session as login_session
-
+from database import *
 app = Flask(__name__)
 app.secret_key = "MY_SUPER_SECRET_KEY"
+@app.home('/')
+def home():
+    return render_template("home.html")
 
+@app.web('/about')
+def about():
+    return render_template("about.html")
+ @app.web('/modecore')
+def store():
+	products= query_all()
+    return render_template("store.html",products=products)
 
 ##### Code here ######
 
